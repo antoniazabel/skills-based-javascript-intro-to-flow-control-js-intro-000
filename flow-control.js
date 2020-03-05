e75d6f23b6228b67e0dae244c51574a7fd84dc0b
Antonia Gavrilis <antoniazabel@gmail.com>
2020-03-05 01:54:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,3 +22,22 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
   return (age > 12 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
+
+function switchAge(age) {
+switch (age) {
+  case 13:
+  return "You are a teenager" 
+      // code to be executed if case n is true
+      break; // break out of switch statement once code executed
+  case 14:
+      // code to be executed if case m is true
+      break; // break out of switch statement once code executed
+  case 15:
+          // code to be executed if case m is true
+      break; // break out of switch statement once code executed
+  default:  // all other cases
+      // code to be executed if case n and case m false
+    }
+}
+
+
